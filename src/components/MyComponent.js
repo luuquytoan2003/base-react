b34f045c7fd80f3b0a0e840bed49061c5ab81deb
LuuQuyToan <luutoan0356@gmail.com>
2024-06-01 17:05:15 +0700

    update

---
+++ src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class MyComponent extends React.Component{
+    render(){
+        return(
+            <div>
+                my firt component
+            </div>
+        );
+    }
+}
+export default MyComponent;
