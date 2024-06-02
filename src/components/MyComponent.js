d4983491fe0c02d56fd1e39fdda7cc585692a4e7
LuuQuyToan <luutoan0356@gmail.com>
2024-06-02 13:03:09 +0700

    update

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,10 +2,44 @@ import React from 'react';
 
 
 class MyComponent extends React.Component{
+
+    state = {
+        name: 'toàn',
+        address: 'hà nội',
+        age: 21
+    }
+    handleClick(event){
+        console.log(">>> Click me my button");
+        this.setState({
+            name: "toLaToan",
+            age: Math.floor((Math.random()*100)+1)
+        })
+    }
+
+    handleOnMouseOver(event){
+        // console.log(event.pageX);
+    }
+
+    handleOnChangeInput(event){
+        this.setState({
+            name: event.target.value,
+        })
+    }
+    handleOnSubmit(event){
+        event.preventDefault();
+        console.log(this.state);
+    }
     render(){
         return(
             <div>
-                my firt component
+                my name is {this.state.name} and i'm {this.state.age}
+                <form onSubmit={(event) => {this.handleOnSubmit(event)}}>
+                    <input 
+                        onChange={(event) => {this.handleOnChangeInput(event)}}
+                        type='text' 
+                    />
+                    <button>Submit</button>
+                </form>
             </div>
         );
     }
