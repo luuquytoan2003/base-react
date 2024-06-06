bd9d8e8951a2f054ee93bd2265a66ec1b1b39100
LuuQuyToan <luutoan0356@gmail.com>
2024-06-06 16:19:16 +0700

    install react-router-dom react-boostrap boostrap

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-import React from 'react';
 import './App.scss';
-import MyComponent from './components/MyComponent';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and lưu quý toàn
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-class App extends React.Component{
-  render(){
-    return(
-      <>
-        hello world với Eric &amp; hỏi dân it
-        <MyComponent title={'hello friend'}/>
-      </>
-    )
-  }
+function App() {
+  return (
+    <div className="app-container">
+      hello world
+    </div>
+  );
 }
 
 export default App;
